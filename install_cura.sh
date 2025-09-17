c16f63847cd5efdda97b09e3e5b956e9982b4739
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-09-17 14:38:11 +0200

    Update install_cura.sh

--- install_cura.sh
+++ install_cura.sh
@@ -1,29 +1,78 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+# Copyright (c) 2025
+# Author: RomainRou
+# License: MIT
+# GitHub: https://github.com/RomainRou/cura-auto
 
-# ----------------- Variables -----------------
-VMID=${VMID:-111}
+function header_info {
+  clear
+  cat <<"EOF"
+   ____                 _             
+  / ___|_ __ __ _ _ __ | | _____ _ __ 
+ | |   | '__/ _` | '_ \| |/ / _ \ '__|
+ | |___| | | (_| | | | |   <  __/ |   
+  \____|_|  \__,_|_| |_|_|\_\___|_|   
+EOF
+}
+
+header_info
+echo -e "\n Loading..."
+
+GEN_MAC=02:$(openssl rand -hex 5 | awk '{print toupper($0)}' | sed 's/\(..\)/\1:/g; s/.$//')
+NEXTID=$(pvesh get /cluster/nextid)
+
+VMID=${VMID:-$NEXTID}
 VMNAME=${VMNAME:-CuraZeroBoot}
-DISK=${DISK:-32}       # taille disque en Go
-MEM=${MEM:-4096}       # mémoire en Mo
+MEM=${MEM:-4096}
 CORES=${CORES:-2}
+DISK=${DISK:-32}       # Go
 BRIDGE=${BRIDGE:-vmbr0}
 USER=${USER:-cura}
 
-# Stockages
-STORAGE_DISK="local-lvm"   # disque LVM thin
-STORAGE_ISO="local"        # dossier pour ISO et preseed
+STORAGE_DISK="local-lvm"
+STORAGE_ISO="local"
 ISO_DIR="/var/lib/vz/template/iso"
 CURA_APPIMAGE_URL="https://download.ultimaker.com/software/Ultimaker_Cura-5.5.0.AppImage"
 
-# ----------------- Préparation ISO -----------------
+set -e
+trap 'error_handler $LINENO "$BASH_COMMAND"' ERR
+
+function error_handler() {
+  local exit_code="$?"
+  local line_number="$1"
+  local command="$2"
+  echo -e "[ERROR] in line $line_number: exit code $exit_code: $command"
+  cleanup_vmid
+}
+
+function cleanup_vmid() {
+  if qm status $VMID &>/dev/null; then
+    qm stop $VMID &>/dev/null
+    qm destroy $VMID &>/dev/null
+  fi
+}
+
+function msg_info() { echo -ne " [..] $1..."; }
+function msg_ok() { echo -e " [✓] $1"; }
+
+check_root() {
+  if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Please run as root."; exit
+  fi
+}
+
+check_root
+
+# ----------------- Téléchargement ISO -----------------
 mkdir -p $ISO_DIR
+msg_info "Downloading Debian ISO"
 ISO_NAME="$(wget -qO- https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -o 'debian-[0-9.]*-amd64-netinst.iso' | head -n1)"
 ISO_URL="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/$ISO_NAME"
 ISO_PATH="$ISO_DIR/$ISO_NAME"
 wget -nc -O $ISO_PATH $ISO_URL
+msg_ok "Downloaded $ISO_NAME"
 
-# ----------------- Préseed Debian -----------------
+# ----------------- Préseed -----------------
 PRESEED="$ISO_DIR/preseed.cfg"
 cat <<EOF > $PRESEED
 d-i debian-installer/locale string en_US
@@ -32,6 +81,7 @@ d-i netcfg/get_hostname string $VMNAME
 d-i netcfg/get_domain string local
 d-i mirror/country string manual
 d-i mirror/http/hostname string ftp.debian.org
+d-i mirror/http/directory string /debian
 d-i passwd/user-fullname string Cura User
 d-i passwd/username string $USER
 d-i passwd/user-password password cura
@@ -42,11 +92,9 @@ d-i partman/confirm boolean true
 d-i partman/confirm_nooverwrite boolean true
 tasksel tasksel/first multiselect standard, ssh-server
 d-i grub-installer/only_debian boolean true
-d-i grub-installer/with_other_os boolean true
 d-i preseed/late_command string in-target bash -c "
 apt update
 apt install -y xorg openbox wget libglu1-mesa libxi6 libxrender1 libxrandr2 libxinerama1
-useradd -m -s /bin/bash $USER || true
 wget -O /home/$USER/Cura.AppImage $CURA_APPIMAGE_URL
 chmod +x /home/$USER/Cura.AppImage
 chown -R $USER:$USER /home/$USER
@@ -65,21 +113,21 @@ echo 'if [ -z "\$DISPLAY" ] && [ "\$(tty)" = "/dev/tty1" ]; then
 fi' >> /home/$USER/.bash_profile
 chown $USER:$USER /home/$USER/.bash_profile
 "
-d-i finish-install/reboot_in_progress note
 EOF
 
 # ----------------- Création VM -----------------
-echo "🖥️ Création de la VM $VMNAME ($VMID) sur $STORAGE_DISK..."
+msg_info "Creating VM $VMNAME ($VMID) on $STORAGE_DISK"
 qm destroy $VMID --purge || true
 qm create $VMID --name $VMNAME --memory $MEM --cores $CORES \
-    --net0 virtio,bridge=$BRIDGE --scsihw virtio-scsi-pci \
-    --scsi0 $STORAGE_DISK:$DISK,format=raw --boot c --bootdisk scsi0
+  --net0 virtio,bridge=$BRIDGE,macaddr=$GEN_MAC --scsihw virtio-scsi-pci \
+  --scsi0 $STORAGE_DISK:$DISK,format=raw --boot c --bootdisk scsi0
+msg_ok "VM Created"
 
 # ----------------- Attachement ISO et Preseed -----------------
 qm set $VMID --ide2 $STORAGE_ISO:iso/$ISO_NAME,media=cdrom
 qm set $VMID --ide3 $STORAGE_ISO:iso/preseed.cfg,media=cdrom
 
 # ----------------- Démarrage VM -----------------
+msg_info "Starting VM $VMNAME"
 qm start $VMID
-
-echo "✅ VM créée et démarrée. Debian + Cura s’installeront automatiquement."
+msg_ok "VM started! Debian + Cura will install automatically"
