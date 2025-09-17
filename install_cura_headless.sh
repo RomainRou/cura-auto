907a1876b7a0d9a690a23f415b53cd8b629bbb74
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-09-17 15:08:36 +0200

    Update install_cura_headless.sh

--- install_cura_headless.sh
+++ install_cura_headless.sh
@@ -6,7 +6,7 @@ set -e
 # ----------------------------
 VMID=9000
 VM_NAME="cura-vm"
-ISO_PATH="/var/lib/vz/template/iso/debian-12.6.0-amd64-netinst.iso"
+ISO_PATH="/var/lib/vz/template/iso/debian-12.6.0-amd64-netinst.iso"  # ISO Debian minimal
 STORAGE="local-lvm"
 DISK_SIZE="20G"
 RAM="2048"
@@ -30,18 +30,25 @@ qm create $VMID \
   --boot c \
   --bootdisk scsi0
 
-qm importdisk $VMID $ISO_PATH $STORAGE
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 $STORAGE:$DISK_SIZE
+# Création du disque LVM correctement
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $STORAGE:${VMID}-disk-0,size=$DISK_SIZE
+
+# Ajout de l'ISO comme CD-ROM pour installation
 qm set $VMID --ide2 $STORAGE:iso,media=cdrom
+
+# Boot et console
 qm set $VMID --boot c --bootdisk scsi0
 qm set $VMID --serial0 socket --vga serial0
 
+echo "[+] VM créée. Démarrez la VM via l'interface web Proxmox pour installer Debian minimal."
+
 # ----------------------------
-# SCRIPT D'INSTALLATION CURA
+# SCRIPT DE SETUP CURA
 # ----------------------------
 cat << 'EOF' > $SNIPPET_DIR/setup_cura.sh
 #!/bin/bash
 set -e
+
 # Mise à jour et dépendances
 apt update && apt upgrade -y
 apt install -y python3-pip python3-pyqt5 python3-setuptools xorg openbox wget sudo
@@ -72,13 +79,11 @@ EOF
 
 chmod +x $SNIPPET_DIR/setup_cura.sh
 
-# ----------------------------
-# Injection du script dans la VM
-# ----------------------------
-qm set $VMID --cicustom "user=local:snippets/setup_cura.sh"
+echo "[+] Script de setup Cura créé. Une fois Debian installé dans la VM, connectez-vous et exécutez :"
+echo "    bash /var/lib/vz/snippets/setup_cura.sh"
 
 # ----------------------------
-# DEMARRAGE DE LA VM
+# LANCEMENT DE LA VM
 # ----------------------------
 qm start $VMID
-echo "[+] VM $VM_NAME (ID $VMID) démarrée. Debian sera installé automatiquement et Cura configuré."
+echo "[+] VM $VM_NAME (ID $VMID) démarrée."
